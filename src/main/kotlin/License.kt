0c438791f9f80b140f3947d848333d5d6e6a9cd6
Kazik Pogoda <morisil@xemantic.com>
2025-01-14 20:32:47 +0100

    The list of OSS licenses

---
+++ src/main/kotlin/License.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 Kazimierz Pogoda / Xemantic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xemantic.gradle
+
+/**
+ * The list of open source licenses which can be used by xemantic projects.
+ */
+enum class License(
+    val id: String,
+    val url: String,
+    val spxdx: String
+) {
+    APACHE(
+        id = "The Apache Software License, Version 2.0",
+        url = "http://www.apache.org/licenses/LICENSE-2.0.txt",
+        spxdx = "Apache-2.0"
+    ),
+    GPL(
+        id = "GNU General Public License, Version 3.0",
+        url = "https://www.gnu.org/licenses/gpl-3.0.txt",
+        spxdx = "GPL-3.0"
+    ),
+    LGPL(
+        id = "GNU Lesser General Public License, Version 3.0",
+        url = "https://www.gnu.org/licenses/lgpl-3.0.txt",
+        spxdx = "LGPL-3.0"
+    )
+}
